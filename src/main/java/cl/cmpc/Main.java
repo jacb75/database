e37c72741ada94ab2ce6f5c2608afac4cbbf174c
jacb75 <jaime.catalanb@gmail.com>
2021-07-29 02:25:21 -0400

    Update Main.java

--- src/main/java/cl/cmpc/Main.java
+++ src/main/java/cl/cmpc/Main.java
@@ -6,13 +6,20 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Connection conn = null;
+        System.out.println("Hello Prueba");
         try {
-            Connection conn = DriverManager.getConnection(args[0],args[1],args[2]);
+            conn = DriverManager.getConnection(args[0],args[1],args[2]);
             System.out.println("Conectados a PostgreSQL");
 
         }catch(SQLException ex) {
             System.err.println("Error en conexion a Base de datos " + ex.getMessage());
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) { /* Ignored */}
+            }
         }
 
     }
